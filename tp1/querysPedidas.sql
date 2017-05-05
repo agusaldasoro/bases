53981bcb5d75afea4029cb4c671b6861e8c80b64
Ezequiel Zimenspitz <ezeqzim@gmail.com>
2017-05-05 20:05:21 -0300

    varias querys

--- tp1/querysPedidas.sql
+++ tp1/querysPedidas.sql
@@ -1,81 +1,146 @@
 -- 1)
-
-SELECT Participante.nombre, Participante.apellido, Participante.dni, CategoriaIndividual.nombreDeModalidad
-FROM Participante
-JOIN Competidor ON Participante.dni = Competidor.dni
-JOIN ParticipaEn ON Competidor.dni = ParticipaEn.dni
-JOIN CategoriaIndividual ON ParticipaEn.idCategoria = CategoriaIndividual.idCategoria
-ORDER BY ParticipaEn.idCategoria;
-
-SELECT nombreDeFantasia
-FROM Equipo
-ORDER BY idCategoria;
+(
+  SELECT Participante.dni AS identificacion, CategoriaIndividual.nombreDeModalidad
+  FROM Participante
+  JOIN Competidor ON Participante.dni = Competidor.dni
+  JOIN ParticipaEn ON Competidor.dni = ParticipaEn.dni
+  JOIN CategoriaIndividual ON ParticipaEn.idCategoria = CategoriaIndividual.idCategoria
+  ORDER BY ParticipaEn.idCategoria
+) UNION (
+  SELECT nombreDeFantasia AS identificacion, CategoriaGrupal.nombreDeModalidad
+  FROM Equipo
+  JOIN CategoriaGrupal ON Equipo.idCategoria = CategoriaGrupal.idCategoria
+  ORDER BY idCategoria
+);
 
 -- 2)
-SELECT Escuela.nombrePais, cantOroInd, cantOroGrup, cantPlataInd, cantPlataGrup, cantBronceInd, cantBronceGrup
-FROM Escuela
-JOIN (
-  SELECT Escuela.nombrePais paisOroInd, COUNT(CategoriaIndividual.dniPrimerPuesto) cantOroInd
-  FROM CategoriaIndividual
-  JOIN Participante ON CategoriaIndividual.dniPrimerPuesto = Participante.dni
-  JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
-  GROUP BY Escuela.nombrePais
-) AS OroInd ON paisOroInd = Escuela.nombrePais
-JOIN (
-  SELECT Escuela.nombrePais paisPlataInd, COUNT(CategoriaIndividual.dniSegundoPuesto) cantPlataInd
-  FROM CategoriaIndividual
-  JOIN Participante ON CategoriaIndividual.dniSegundoPuesto = Participante.dni
-  JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
-  GROUP BY Escuela.nombrePais
-) AS PlataInd ON paisPlataInd = Escuela.nombrePais
-JOIN (
-  SELECT Escuela.nombrePais paisBronceInd, COUNT(CategoriaIndividual.dniTercerPuesto) cantBronceInd
-  FROM CategoriaIndividual
-  JOIN Participante ON CategoriaIndividual.dniTercerPuesto = Participante.dni
-  JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
-  GROUP BY Escuela.nombrePais
-) AS BronceInd ON paisBronceInd = Escuela.nombrePais
-JOIN (
-  SELECT Escuela.nombrePais paisOroGrup, COUNT(DISTINCT CategoriaGrupal.nombrePrimerPuesto) cantOroGrup
-  FROM CategoriaGrupal
-  JOIN Equipo ON CategoriaGrupal.nombrePrimerPuesto = Equipo.nombreDeFantasia
-  JOIN Competidor ON Equipo.nombreDeFantasia = Competidor.titular OR Equipo.nombreDeFantasia = Competidor.suplente
-  JOIN Participante ON Competidor.dni = Participante.dni
-  JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
-  GROUP BY Escuela.nombrePais
-) AS OroGrup ON paisOroGrup = Escuela.nombrePais
-JOIN (
-  SELECT Escuela.nombrePais paisPlataGrup, COUNT(DISTINCT CategoriaGrupal.nombreSegundoPuesto) cantPlataGrup
-  FROM CategoriaGrupal
-  JOIN Equipo ON CategoriaGrupal.nombreSegundoPuesto = Equipo.nombreDeFantasia
-  JOIN Competidor ON Equipo.nombreDeFantasia = Competidor.titular OR Equipo.nombreDeFantasia = Competidor.suplente
-  JOIN Participante ON Competidor.dni = Participante.dni
-  JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
-  GROUP BY Escuela.nombrePais
-) AS PlataGrup ON paisPlataGrup = Escuela.nombrePais
-JOIN (
-  SELECT Escuela.nombrePais paisBronceGrup, COUNT(DISTINCT CategoriaGrupal.nombreTercerPuesto) cantBronceGrup
-  FROM CategoriaGrupal
-  JOIN Equipo ON CategoriaGrupal.nombreTercerPuesto = Equipo.nombreDeFantasia
-  JOIN Competidor ON Equipo.nombreDeFantasia = Competidor.titular OR Equipo.nombreDeFantasia = Competidor.suplente
-  JOIN Participante ON Competidor.dni = Participante.dni
-  JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
-  GROUP BY Escuela.nombrePais
-) AS BronceGrup ON paisBronceGrup = Escuela.nombrePais
-GROUP BY Escuela.nombrePais
-HAVING MAX(cantOroInd + cantOroGrup + cantPlataInd + cantPlataGrup + cantBronceInd + cantBronceGrup);
-
-/*
-SELECT Pais.nombre
-FROM Pais
-JOIN Escuela ON Pais.nombre = Escuela.nombrePais
-JOIN Participante ON Escuela.nombre = Participante.nombreDeEscuela
-JOIN Competidor ON Participante.dni = Competidor.dni
-*/
+SELECT nombrePais, COUNT(nombrePais) AS cantidad
+FROM (
+    SELECT Escuela.nombrePais
+    FROM (
+       SELECT dniPrimerPuesto AS dni
+        FROM CategoriaIndividual
+      UNION ALL
+        SELECT dniSegundoPuesto AS dni
+        FROM CategoriaIndividual
+      UNION ALL
+        SELECT dniTercerPuesto AS dni
+        FROM CategoriaIndividual
+    ) AS Dnis
+    JOIN Participante ON Participante.dni = Dnis.dni
+    JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
+  UNION ALL
+    SELECT Escuela.nombrePais
+    FROM (
+        SELECT nombrePrimerPuesto AS nombre
+        FROM CategoriaGrupal
+      UNION ALL
+        SELECT nombreSegundoPuesto AS nombre
+        FROM CategoriaGrupal
+      UNION ALL
+        SELECT nombreTercerPuesto AS nombre
+        FROM CategoriaGrupal
+    ) AS Nombres
+    JOIN Competidor ON Competidor.titular = Nombres.nombre
+    JOIN Participante ON Competidor.dni = Participante.dni
+    JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
+    GROUP BY Nombres.nombre, Escuela.nombrePais
+) AS Paises
+GROUP BY nombrePais
+ORDER BY cantidad DESC
+LIMIT 1;
 
 -- 3)
+SELECT nombrePais, SUM(puntaje) AS puntajeTotal
+FROM (
+    SELECT Escuela.nombrePais, puntaje
+    FROM (
+       SELECT dniPrimerPuesto AS dni, 3 AS puntaje
+        FROM CategoriaIndividual
+      UNION ALL
+        SELECT dniSegundoPuesto AS dni, 2 AS puntaje
+        FROM CategoriaIndividual
+      UNION ALL
+        SELECT dniTercerPuesto AS dni, 1 AS puntaje
+        FROM CategoriaIndividual
+    ) AS Dnis
+    JOIN Participante ON Participante.dni = Dnis.dni
+    JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
+  UNION ALL
+    SELECT Escuela.nombrePais, puntaje
+    FROM (
+        SELECT nombrePrimerPuesto AS nombre, 3 AS puntaje
+        FROM CategoriaGrupal
+      UNION ALL
+        SELECT nombreSegundoPuesto AS nombre, 2 AS puntaje
+        FROM CategoriaGrupal
+      UNION ALL
+        SELECT nombreTercerPuesto AS nombre, 1 AS puntaje
+        FROM CategoriaGrupal
+    ) AS Nombres
+    JOIN Competidor ON Competidor.titular = Nombres.nombre
+    JOIN Participante ON Competidor.dni = Participante.dni
+    JOIN Escuela ON Participante.nombreDeEscuela = Escuela.nombre
+    GROUP BY Nombres.nombre, Escuela.nombrePais
+) AS EscuelasYPuntaje
+GROUP BY nombrePais
+ORDER BY puntajeTotal DESC;
 
 -- 4)
+  SELECT dniPrimerPuesto AS dni, idCategoria, "Primero"
+  FROM CategoriaIndividual
+  WHERE dniPrimerPuesto IS NOT NULL
+UNION ALL
+  SELECT dniSegundoPuesto AS dni, idCategoria, "Segundo"
+  FROM CategoriaIndividual
+  WHERE dniSegundoPuesto IS NOT NULL
+UNION ALL
+  SELECT dniTercerPuesto AS dni, idCategoria, "Tercero"
+  FROM CategoriaIndividual
+  WHERE dniTercerPuesto IS NOT NULL
+UNION ALL
+  SELECT Competidor.dni, CategoriaIndividual.idCategoria, "No llega al podio"
+  FROM Competidor
+  JOIN ParticipaEn ON Competidor.dni = ParticipaEn.dni
+  JOIN CategoriaIndividual ON ParticipaEn.idCategoria = CategoriaIndividual.idCategoria
+  WHERE NOT (
+    Competidor.dni = CategoriaIndividual.dniPrimerPuesto
+    OR
+    Competidor.dni = CategoriaIndividual.dniSegundoPuesto
+    OR
+    Competidor.dni = CategoriaIndividual.dniTercerPuesto
+  )
+UNION ALL
+  SELECT Competidor.dni AS dni, CategoriaGrupal.idCategoria, "Primero"
+  FROM Competidor
+  JOIN Equipo ON Equipo.nombreDeFantasia = Competidor.titular OR Equipo.nombreDeFantasia = Competidor.suplente
+  JOIN CategoriaGrupal ON Equipo.idCategoria = CategoriaGrupal.idCategoria
+  WHERE nombrePrimerPuesto IS NOT NULL
+UNION ALL
+  SELECT Competidor.dni AS dni, CategoriaGrupal.idCategoria, "Segundo"
+  FROM Competidor
+  JOIN Equipo ON Equipo.nombreDeFantasia = Competidor.titular OR Equipo.nombreDeFantasia = Competidor.suplente
+  JOIN CategoriaGrupal ON Equipo.idCategoria = CategoriaGrupal.idCategoria
+  WHERE nombreSegundoPuesto IS NOT NULL
+UNION ALL
+  SELECT Competidor.dni AS dni, CategoriaGrupal.idCategoria, "Tercero"
+  FROM Competidor
+  JOIN Equipo ON Equipo.nombreDeFantasia = Competidor.titular OR Equipo.nombreDeFantasia = Competidor.suplente
+  JOIN CategoriaGrupal ON Equipo.idCategoria = CategoriaGrupal.idCategoria
+  WHERE nombreTercerPuesto IS NOT NULL
+UNION ALL
+  SELECT Competidor.dni, CategoriaGrupal.idCategoria, "No llega al podio"
+  FROM Competidor
+  JOIN Equipo ON Competidor.titular = Equipo.nombreDeFantasia OR Competidor.suplente = Equipo.nombreDeFantasia
+  JOIN CategoriaGrupal ON Equipo.idCategoria = CategoriaGrupal.idCategoria
+  WHERE NOT (
+    Equipo.idCategoria = CategoriaGrupal.nombrePrimerPuesto
+    OR
+    Equipo.idCategoria = CategoriaGrupal.nombreSegundoPuesto
+    OR
+    Equipo.idCategoria = CategoriaGrupal.nombreTercerPuesto
+  )
+ORDER BY idCategoria, dni
 
 -- 5)
 
